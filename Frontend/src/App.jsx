d529eeb7c262fbce79e9c1e4bd8c9bf44606c1e8
Irfan <irfanyusufk13@gmail.com>
2024-11-11 20:51:20 +0700

    fix app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,18 @@
-import "./App.css";
-
 import React from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import BikeInUse from "./pages/BikeInUse";
 import ParkingStatus from "./pages/ParkingStatus";
 import History from "./pages/History";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import "./App.css";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
@@ -23,4 +23,5 @@ function App() {
     </Router>
   );
 }
+
 export default App;
